999795be4fa79ccdad3fa7fc046b9ae79869fc44
Adam Reyner <reyneradam@gmail.com>
2020-11-22 17:13:02 +0700

    mbuh

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,24 +10,24 @@
                <a href="/blog"> 
                   <div class="banner-item-01">
                     <div class="text-content">
-                    <h4>Pelajari Lebih Banyak Blog Kami</h4>
-                    <h2>BLOG</h2>
+                    <h1 class="text-dark" data-aos="fade-up">Lihat Artikel Kami</h1>
+                    <h1 class="custom-btn btn-bg btn mt-3">BLOG</h2>
                     </div>
                   </div>
                </a>
                <a href="/video"> 
                   <div class="banner-item-02">
                     <div class="text-content">
-                    <h4>Lihat Video Dokumentasi Kami</h4>
-                    <h2>VIDEO</h2>
+                    <h1 class="text-dark" data-aos="fade-up">Lihat Video Dokumentasi Kami</h1>
+                    <h2 class="custom-btn btn-bg btn mt-3">VIDEO</h2>
                     </div>
                   </div>
                </a>
                <a href="/livestream">
                   <div class="banner-item-03">
                     <div class="text-content">
-                    <h4>Lihat Tayangan Langsung yang Kami Siarkan</h4>
-                    <h2>LIVE STREAMING</h2>
+                    <h1 class="text-dark" data-aos="fade-up">Lihat Tayangan Langsung yang Kami Siarkan</h1>
+                    <h2 class="custom-btn btn-bg btn mt-3">LIVE STREAMING</h2>
                     </div>
                   </div>
                </a>
@@ -38,7 +38,26 @@
 
     
 
+     <!-- ABOUT -->
+     <section class="about section-padding pb-0" id="about">           
 
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-lg-7 mx-auto col-md-10 col-12">
+                         <div class="about-info">
+
+                              <h2 class="mb-4" data-aos="fade-up">the best <strong>Digital Marketing agency</strong> in Rio de Janeiro</h2>
+
+                              <p class="mb-0" data-aos="fade-up">Total 5 HTML pages are included in this template from TemplateMo website. Please check 2 <a href="blog.html">blog</a> pages, <a href="project-detail.html">project</a> page, and <a href="contact.html">contact</a> page. 
+                              <br><br>You are <strong>allowed</strong> to use this template for commercial or non-commercial purpose. You are NOT allowed to redistribute the template ZIP file on template collection websites.</p>
+                         </div>                    </div>
+               </div>
+          </div>
+          <div class="container col-12">
+               <img src="/images/tentang-kami.jpg" data-aos="fade-up" class="img-fluid mt-5 mx-auto d-block" alt="office">
+          </div>
+     </section>
    
 
      <!-- Video -->
@@ -135,26 +154,6 @@
           </div>
      </section>
 
-
-
-     <!-- ABOUT -->
-     <section class="about" id="about">
-          <div class="container">
-               <div class="row">
-
-                    <div class="col-lg-7 mx-auto col-md-10 col-12">
-                         <div class="about-info">
-
-                              <h2 class="mb-4" data-aos="fade-up"><strong>Tentang Kami</strong> </h2>
-
-                              <p class="justify"  data-aos="fade-up">Total 5 HTML pages are included in this template from TemplateMo website. Please check 2 <a href="blog.html">blog</a> pages, <a href="project-detail.html">project</a> page, and <a href="contact.html">contact</a> page. 
-                              <br><br>You are <strong>allowed</strong> to use this template for commercial or non-commercial purpose. You are NOT allowed to redistribute the template ZIP file on template collection websites.</p>
-                         </div>
-                    </div>
-
-               </div>
-          </div>
-     </section>
 
 @endsection
 
